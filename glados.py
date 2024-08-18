04431a3b87fcb7efc759829e0051f5fc143ef136
Zhou Jiale <69026015+Zhou1317fe5@users.noreply.github.com>
2024-08-19 07:43:20 +0800

    Update glados.py

--- glados.py
+++ glados.py
@@ -33,11 +33,13 @@ if __name__ == '__main__':
             print("签到成功") # 日志输出
             print(email+'--结果--'+mess+'--剩余('+time+')天')  # 日志输出
             sendContent += email+'--'+mess+'--剩余('+time+')天\n'
+            print("sendContent:"+sendContent) # 日志输出
         else:
             requests.get('http://www.pushplus.plus/send?token=' + sckey + '&content='+email+'cookie已失效')
             print('cookie已失效')  # 日志输出
      #--------------------------------------------------------------------------------------------------------#   
     if sckey != "":
-         requests.get('http://www.pushplus.plus/send?token=' + sckey + '&title='+email+'签到成功'+'&content='+sendContent)
+        requests.get('http://www.pushplus.plus/send?token=' + sckey + '&title='+email+'签到成功'+'&content='+sendContent)
+        print("发送成功") # 日志输出
 
 
